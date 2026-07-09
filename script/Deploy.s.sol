208e232cad7eea0406f57a4851aba34d852e7938
0xNick <3832465+0xNikolas@users.noreply.github.com>
2026-07-09 19:25:17 +0300

    refactor(script): source broadcaster from CLI instead of env vars
    Drop the ETH_FROM/MNEMONIC constructor machinery in BaseScript and use an
    argument-less vm.startBroadcast(), so the broadcaster is supplied at deploy
    time via --private-key or --account. Log the active broadcaster in Deploy.

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.29 <0.9.0;
 
+import { console } from "forge-std/src/console.sol";
+
 import { Foo } from "../src/Foo.sol";
 
 import { BaseScript } from "./Base.s.sol";
@@ -8,6 +10,9 @@ import { BaseScript } from "./Base.s.sol";
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/guides/scripting-with-solidity
 contract Deploy is BaseScript {
     function run() public broadcast returns (Foo foo) {
+        (, address broadcaster,) = vm.readCallers();
+        console.log("Broadcasting from: ", broadcaster);
+
         foo = new Foo();
     }
 }
